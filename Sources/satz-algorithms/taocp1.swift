f0d88c421dfec8c2e5915c59cf84852270e64ad7
Lie Yan <robyn.lie.yan@outlook.com>
2024-09-10 10:05:55 +0800

    feat: add gcd()

---
+++ Sources/satz-algorithms/taocp1.swift
@@ -0,0 +1,20 @@
+// Copyright 2024 Lie Yan
+
+/// Given two positive integers m and n, find their greatest common divisor.
+public func gcd<T: FixedWidthInteger>(_ m: T, _ n: T) -> T {
+    precondition(m > 0 && n > 0)
+
+    var m = m
+    var n = n
+
+    while true {
+        m = m % n
+        if m == 0 {
+            return n
+        }
+        n = n % m
+        if n == 0 {
+            return m
+        }
+    }
+}
---
+++ Tests/satz-algorithms-tests/Taocp1Tests.swift
@@ -0,0 +1,13 @@
+// Copyright 2024 Lie Yan
+
+@testable import satz_algorithms
+import XCTest
+
+final class Taocp1Tests: XCTestCase {
+    func testGcd() throws {
+        XCTAssertEqual(gcd(119, 544), 17)
+        XCTAssertEqual(gcd(544, 119), 17)
+        XCTAssertEqual(gcd(2166, 6099), 57)
+        XCTAssertEqual(gcd(6099, 2166), 57)
+    }
+}
