76713b8b966c1968d6428953edbff21ea4c88fbc
Lie Yan <robyn.lie.yan@outlook.com>
2024-09-11 13:03:03 +0800

    polish

--- Sources/satz-algorithms/taocp1.swift
+++ Sources/satz-algorithms/taocp1.swift
@@ -10,14 +10,11 @@ public func gcd<T>(_ m: T, _ n: T) -> T
     var n = n
 
     while true {
-        m = m % n
-        if m == 0 {
+        let r = m % n
+        if r == 0 {
             return n
         }
-        n = n % m
-        if n == 0 {
-            return m
-        }
+        (m, n) = (n, r)
     }
 }
 
@@ -42,13 +39,8 @@ public func gcd_extended<T>(_ m: T, _ n: T) -> (d: T, a: T, b: T)
             return (d, a, b)
         }
 
-        c = d
-        d = r
-        var t = aa
-        aa = a
-        a = t - q * a
-        t = bb
-        bb = b
-        b = t - q * b
+        (c, d) = (d, r)
+        (aa, a) = (a, aa - q * a)
+        (bb, b) = (b, bb - q * b)
     }
 }
--- Tests/satz-algorithms-tests/Taocp1Tests.swift
+++ Tests/satz-algorithms-tests/Taocp1Tests.swift
@@ -12,8 +12,24 @@ final class Taocp1Tests: XCTestCase {
     }
 
     func test_gcd_extended() {
-        for (m, n) in [(119, 544), (544, 119), (2166, 6099), (6099, 2166)] {
+        do {
+            let (m, n) = (1769, 551)
             let (d, a, b) = gcd_extended(m, n)
+            XCTAssertEqual(d, 29)
+            XCTAssertEqual(a, 5)
+            XCTAssertEqual(b, -16)
+            XCTAssertEqual(a * m + b * n, d)
+        }
+
+        for (m, n) in [(119, 544), (544, 119)] {
+            let (d, a, b) = gcd_extended(m, n)
+            XCTAssertEqual(d, 17)
+            XCTAssertEqual(a * m + b * n, d)
+        }
+
+        for (m, n) in [(2166, 6099), (6099, 2166)] {
+            let (d, a, b) = gcd_extended(m, n)
+            XCTAssertEqual(d, 57)
             XCTAssertEqual(a * m + b * n, d)
         }
     }
