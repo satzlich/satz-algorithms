8844101010c99c8a0aba3482353e486122598434
Lie Yan <robyn.lie.yan@outlook.com>
2024-09-11 12:51:22 +0800

    feat: add gcd_extended() (#2)

--- Sources/satz-algorithms/taocp1.swift
+++ Sources/satz-algorithms/taocp1.swift
@@ -1,7 +1,10 @@
 // Copyright 2024 Lie Yan
 
 /// Given two positive integers m and n, find their greatest common divisor.
-public func gcd<T: FixedWidthInteger>(_ m: T, _ n: T) -> T {
+@inlinable
+public func gcd<T>(_ m: T, _ n: T) -> T
+    where T: BinaryInteger
+{
     precondition(m > 0 && n > 0)
 
     var m = m
@@ -16,5 +19,37 @@ public func gcd<T: FixedWidthInteger>(_ m: T, _ n: T) -> T {
         if n == 0 {
             return m
         }
+    }
+}
+
+/// Given two positive integers m and n, we compute their greatest common
+/// divisor d, and we also compute two not-necessarily-positive integers a and b
+/// such that am + bn = d.
+public func gcd_extended<T>(_ m: T, _ n: T) -> (d: T, a: T, b: T)
+    where T: SignedInteger
+{
+    precondition(m > 0 && n > 0)
+
+    var aa: T = 1
+    var b: T = 1
+    var a: T = 0
+    var bb: T = 0
+    var c = m
+    var d = n
+
+    while true {
+        let (q, r) = c.quotientAndRemainder(dividingBy: d)
+        if r == 0 {
+            return (d, a, b)
+        }
+
+        c = d
+        d = r
+        var t = aa
+        aa = a
+        a = t - q * a
+        t = bb
+        bb = b
+        b = t - q * b
     }
 }
--- Tests/satz-algorithms-tests/Taocp1Tests.swift
+++ Tests/satz-algorithms-tests/Taocp1Tests.swift
@@ -1,13 +1,29 @@
 // Copyright 2024 Lie Yan
 
-@testable import satz_algorithms
+import satz_algorithms
 import XCTest
 
 final class Taocp1Tests: XCTestCase {
-    func testGcd() throws {
+    func test_gcd() {
         XCTAssertEqual(gcd(119, 544), 17)
         XCTAssertEqual(gcd(544, 119), 17)
         XCTAssertEqual(gcd(2166, 6099), 57)
         XCTAssertEqual(gcd(6099, 2166), 57)
+    }
+
+    func test_gcd_extended() {
+        do {
+            let res = gcd_extended(1769, 551)
+            XCTAssertEqual(res.d, 29)
+            XCTAssertEqual(res.a, 5)
+            XCTAssertEqual(res.b, -16)
+        }
+
+        do {
+            let res = gcd_extended(551, 1769)
+            XCTAssertEqual(res.d, 29)
+            XCTAssertEqual(res.a, -16)
+            XCTAssertEqual(res.b, 5)
+        }
     }
 }
