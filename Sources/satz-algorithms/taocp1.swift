15a6cc9e35e9a4ceb98a435f89c752425d67d1fb
Lie Yan <robyn.lie.yan@outlook.com>
2024-09-11 12:39:26 +0800

    feat: add gcd_extended

--- Sources/satz-algorithms/taocp1.swift
+++ Sources/satz-algorithms/taocp1.swift
@@ -21,3 +21,35 @@ public func gcd<T>(_ m: T, _ n: T) -> T
         }
     }
 }
+
+/// Given two positive integers m and n, we compute their greatest common
+/// divisor d, and we also compute two not-necessarily-positive integers a and b
+/// such that am + bn = d.
+public func gcd_extended<T>(_ m: T, _ n: T) -> (d: T, a: T, b: T)
+    where T: SignedInteger
+{
+    precondition(m > 0 && n > 0)
+
+    var aa: T = 1
+    var b: T = 1
+    var a: T = 0
+    var bb: T = 0
+    var c = m
+    var d = n
+
+    while true {
+        let (q, r) = c.quotientAndRemainder(dividingBy: d)
+        if r == 0 {
+            return (d, a, b)
+        }
+
+        c = d
+        d = r
+        var t = aa
+        aa = a
+        a = t - q * a
+        t = bb
+        bb = b
+        b = t - q * b
+    }
+}
--- Tests/satz-algorithms-tests/Taocp1Tests.swift
+++ Tests/satz-algorithms-tests/Taocp1Tests.swift
@@ -10,4 +10,20 @@ final class Taocp1Tests: XCTestCase {
         XCTAssertEqual(gcd(2166, 6099), 57)
         XCTAssertEqual(gcd(6099, 2166), 57)
     }
+
+    func test_gcd_extended() {
+        do {
+            let res = gcd_extended(1769, 551)
+            XCTAssertEqual(res.d, 29)
+            XCTAssertEqual(res.a, 5)
+            XCTAssertEqual(res.b, -16)
+        }
+
+        do {
+            let res = gcd_extended(551, 1769)
+            XCTAssertEqual(res.d, 29)
+            XCTAssertEqual(res.a, -16)
+            XCTAssertEqual(res.b, 5)
+        }
+    }
 }
