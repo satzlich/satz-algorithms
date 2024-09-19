68398006bc4c9ac900822537133879e091387188
Lie Yan <robyn.lie.yan@outlook.com>
2024-09-19 12:45:38 +0800

    refactor: test using SwiftTesting

--- Tests/satz-algorithms-tests/Taocp1Tests.swift
+++ Tests/satz-algorithms-tests/Taocp1Tests.swift
@@ -1,32 +1,34 @@
 // Copyright 2024 Lie Yan
 
 import satz_algorithms
-import XCTest
+import Testing
 
-final class Taocp1Tests: XCTestCase {
+struct Taocp1Tests {
+    @Test
     func test_gcd() {
-        XCTAssertEqual(gcd(119, 544), 17)
-        XCTAssertEqual(gcd(544, 119), 17)
-        XCTAssertEqual(gcd(2166, 6099), 57)
-        XCTAssertEqual(gcd(6099, 2166), 57)
+        #expect(gcd(119, 544) == 17)
+        #expect(gcd(544, 119) == 17)
+        #expect(gcd(2166, 6099) == 57)
+        #expect(gcd(6099, 2166) == 57)
     }
 
+    @Test
     func test_gcd_extended() {
         do {
             let (m, n) = (1769, 551)
             let (d, a, b) = gcd_extended(m, n)
-            XCTAssertEqual(d, 29)
-            XCTAssertEqual(a, 5)
-            XCTAssertEqual(b, -16)
-            XCTAssertEqual(a * m + b * n, d)
+            #expect(d == 29)
+            #expect(a == 5)
+            #expect(b == -16)
+            #expect(a * m + b * n == d)
         }
 
         for (m, n, dd) in [(119, 544, 17), (544, 119, 17),
                            (2166, 6099, 57), (6099, 2166, 57)]
         {
             let (d, a, b) = gcd_extended(m, n)
-            XCTAssertEqual(d, dd)
-            XCTAssertEqual(a * m + b * n, d)
+            #expect(d == dd)
+            #expect(a * m + b * n == d)
         }
     }
 }
